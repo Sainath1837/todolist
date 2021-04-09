f19e5d4f46ac07b3887a59aba2690cc01e5c8a8f
Chenchunaidu Maddineni <chenchunaidu@iotric.com>
2021-04-09 16:41:14 +0530

    added tododata

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from "react"
+import todosData from "./todosData"
 import TodoItem from "./TodoItem"
 
 function App() {
+    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
     return(
     <div className="todo-List">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todoItems}
     </div>
     )
 }
