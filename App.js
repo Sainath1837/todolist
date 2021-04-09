c95cfc446a3c7e3b5c8fdf4dbcbbe39dc84f4832
Chenchunaidu Maddineni <chenchunaidu@iotric.com>
2021-04-09 12:30:24 +0530

    added todoitem

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from "react"
+import TodoItem from "./TodoItem"
 
 function App() {
     return(
-    <div>
-        <input type="checkbox" />
-        <p>placeholder text</p>
-
-        <input type="checkbox" />
-        <p>placeholder text</p>
-
-        <input type="checkbox" />
-        <p>placeholder text</p>
-
-        <input type="checkbox" />
-        <p>placeholder text</p>
+    <div className="todo-List">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
     </div>
     )
 }
---
+++ TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TodoItem () {
+    return(
+        <div className="todo-item">
+            <input type="checkbox" />
+            <p>placeholder text</p>
+        </div>
+    )    
+
+}
+export default TodoItem
